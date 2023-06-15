7b8d07bf4be86334d31b32fb6b09921c509452b2
KenNyagz <kennyagz681@gmail.com>
2023-06-15 23:25:30 +0300

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-/* Function 
- * for typing 
- * programming description*/
+/*Function for typing programming description*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
