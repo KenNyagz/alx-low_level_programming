83ebb61182a24f9b2e02ca620b01f8e65946bc4f
KenNyagz <kennyagz681@gmail.com>
2023-06-16 19:49:33 +0300

    Last digit selecta

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,13 +14,12 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-int rem = n % 10;
 
-if (rem > 5)
+if (n % 10 > 5)
 {
 printf("Last digit of %d is %d and is greater than 5", n, rem);
 }
-else if (rem == 0)
+else if (n % 10 == 0)
 {
 printf("Last digit of %d is and 0 and is  0", n);
 }
