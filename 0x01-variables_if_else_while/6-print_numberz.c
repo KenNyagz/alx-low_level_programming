f4fbc01628d4f09253d1bcf47746a4c82991703d
KenNyagz <kennyagz681@gmail.com>
2023-06-17 00:04:46 +0300

    comm

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - entry point
+*
+*Return: always 0
+*/
+
+int main(void)
+{
+int Char;
+for (Char = 97; Char <= 122; Char++)
+{
+putchar(Char);
+}
+return (0);
+}
