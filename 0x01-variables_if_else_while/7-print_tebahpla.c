01ea629d613ff50ef4f208f5097d4ff25d331f35
KenNyagz <kennyagz681@gmail.com>
2023-06-17 00:40:52 +0300

    comm

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - entry point
+*Prints lowercase alphabet in reverse
+*Return: Always 0
+*/
+
+int main(void)
+{
+int c;
+for (c = 122; c > 96; c--)
+{
+putchar(c);
+}
+putchar('\n');
+}
