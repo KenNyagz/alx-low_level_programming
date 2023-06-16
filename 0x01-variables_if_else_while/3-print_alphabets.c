da38deb87bd82e2f35dbddb85e02be54ed55fa15
KenNyagz <kennyagz681@gmail.com>
2023-06-16 21:54:59 +0300

    Putchar again with caps and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *Prints alphabet in lowrcase then uppercse
+ *Return: Always 0
+ */
+
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+for (c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+
+
+}
