688d92ebdbc431c5bccf53a69c2ed9c34da612dd
KenNyagz <kennyagz681@gmail.com>
2023-06-16 22:47:53 +0300

    comm

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - entry point
+*print single digit numbers of base 10
+*Return: Always 0
+*/
+
+int main(void)
+{
+printf({1..9{)
+
+
+
+
+
+
+
+}
