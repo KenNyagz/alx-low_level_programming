d15eedca326688b1054d1c28ee8b0946936d52ac
KenNyagz <kennyagz681@gmail.com>
2023-06-17 01:30:55 +0300

    comm

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main -entry point
+*Print all numbers of base 16 in lowercase
+*Return: always 0
+*/
+
+int main(void)
+{
+int i;
+char c;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
+}
+return (0);
+}
