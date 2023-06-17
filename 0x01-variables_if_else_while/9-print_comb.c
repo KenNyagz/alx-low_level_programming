9ba41197953b94d9c46d7f7dc60a2fd5efc7723f
KenNyagz <kennyagz681@gmail.com>
2023-06-17 12:24:50 +0300

    dcdsc

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - entry point
+*print all single digits separated by comma
+*Return: Always 0
+*/
+
+int main(void)
+{
+int i;
+for (i = 0; i <= 9 ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
