47b7d82971eb7dc89031eb59df24675f0ce86ad6
KenNyagz <kennyagz681@gmail.com>
2023-06-17 16:55:31 +0300

    This is ....

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,34 @@
-i#include <stdlib.h>
+#include <stdlib.h>
 #include <stdio.h>
 
- /**
- *main - enrty point
- *Print all tw digit combinations separated by , and space
- *Return: Always 0
- */
- int main(void)
- {
- int i;
- int j;
- for (i = 49; i <= 57; i++)
- {
-   for (j = 49; j <= 57; j++)
-   {
-    if (i = j)
-      {
-       
-       putchar(i);
-       putchar(j);
-       putchar(',');
-       putchar(' ');
-     
-      }
-   }
- putchar('\n');
- return (0);
+/**
+*main - enrty point
+*Print all tw digit combinations separated by , and space
+*Return: Always 0
+*/
 
+int main(void)
+{
+int i;
+int j;
+for (i = 48; i <= 57; i++)
+{
+  for (j = 49; j <= 57; j++)
+  {
+    if (i == j || i == i)
+    {
+      for (i = 49; i <= 57; i++)
+      {     
+        putchar(i);
+        putchar(j);
+        putchar(',');
+        putchar(' ');
+    
+      }
+    }
  }
+putchar('\n');
+return (0);
+
+}
 }
