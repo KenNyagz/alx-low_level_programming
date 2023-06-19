94d56e4ebe6fa605f84b3a0c696a103d38b29873
KenNyagz <kennyagz681@gmail.com>
2023-06-19 15:55:07 +0300

    An aliased Commit to save time

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+*main- entry point
+*Code prints Three digit combinations 
+*without repeating numbers in different combinations or
+*with similar digits
+*Return: Always 0
+*/
 int main(void)
 {
-    int i;
-    int j;
-    int k;
+int i;
+int j;
+int k;
 
-    for (i=48; i<58; i++)
-    {
-        for (j =i+1 ; j<58; j++)
-        {
-	    for (k=j+1; k<58; k++)
-             
-            {
-                putchar(i);
-                putchar(j);
-                putchar(k);
-                if(i==55 && j==56 && k==57)
-                break;	        
-                putchar(','); 
-                putchar(' ');            
-            }    
+for (i=48; i<58; i++)
+{
+for (j =i+1 ; j<58; j++)
+{
+for (k=j+1; k<58; k++)
             
-        }    
-    }
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if(i==55 && j==56 && k==57)
+break;	        
+putchar(','); 
+putchar(' ');            
+}    
+          
+}    
+}
 putchar('\n');  
 return 0;
 }
