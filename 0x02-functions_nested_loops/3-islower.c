4e983713ea47e86d45a89dee0bec4863ef490b59
KenNyagz <kennyagz681@gmail.com>
2023-06-20 23:51:31 +0300

    An aliased Commit to save time

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,22 +2,18 @@
 
 /**
 *_islower - checks for lowercase character
+*@c: is arg to be tested
 *Return: 0 if c is lower case; 1 if not
 *
 */
 
 int _islower(int c)
 {
-/**
-*_islower: check for alphabet case
-*add c is argument
-*add 97 is small 'a'
-*add 122 is small 'z'
-*return:1 if lower; 0 if otherwise
-*/
+
 if (c > 97 && c < 122)
 return (1);
 else
+
 return (0);
 }
 
