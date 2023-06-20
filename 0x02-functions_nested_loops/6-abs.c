09f9b6a474a0cb28c997b85eb6d34f5b31267108
KenNyagz <kennyagz681@gmail.com>
2023-06-21 02:09:45 +0300

    absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_abs - returns absolute value of a number
+*@i: int checked
+*Return: i or i negated if negative number
+*/
+
+int _abs(int)
+{
+int i;
+if (i < 0)
+{
+i = (i * -1);
+return (i);
+}
+else
+return (i);
+}
