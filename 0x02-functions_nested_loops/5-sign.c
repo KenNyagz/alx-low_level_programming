87ba8f60eefcd297fa7e8cd9576c37fcab64fd21
KenNyagz <kennyagz681@gmail.com>
2023-06-21 01:21:44 +0300

    PRINTING NUMBER +VE/-VE

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*print_sign -  prints the sign of a number
+*@n:number being checked
+*Return: 0 if 0, + if positive number,- if negative
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+putchar('+');
+return (1);
+}
+else if (n = 0)
+{
+putchar('0');
+return (0);
+}
+else
+{
+print('-'{;
+return (-1);
+}
+
+}
