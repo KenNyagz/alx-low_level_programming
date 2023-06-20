586f03bef21051bf6c0f7559d2298ac905bf09bd
KenNyagz <kennyagz681@gmail.com>
2023-06-20 20:30:41 +0300

    An aliased Commit to save time

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,12 @@
 #include "main.h"
 
 /**
-*main - entry point
 *print the lower alphabet using our _putchar
-*function in the header
 *Return:Always 0
 */
 
 void print_alphabet(void)
-/**
-*add C
-*
-*Return: void
-*/
+
 {
 char c;
 for (c = 97; c <= 122; c++)
