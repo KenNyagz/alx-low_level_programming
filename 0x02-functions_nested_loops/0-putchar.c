c3c016d5503ae5e51a4ecc0e8537cbbb288f66cf
KenNyagz <kennyagz681@gmail.com>
2023-06-20 18:38:58 +0300

    An aliased Commit to save time

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-*main -entry point
-*printing putchar
-*Return: Always 0
-*/
+4 *main -entry point
+5 *printing putchar
+6 *Return: Always 0
+7 */
 
 int main(void)
 {
-
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
 
 return (0);
 }
