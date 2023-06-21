f084c1243a0f95f3d371a4ea295a5ca826ee17b8
KenNyagz <kennyagz681@gmail.com>
2023-06-21 22:28:29 +0300

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,18 @@
 
 /**
 *print_last_digit - prints the last digit of a number
-*
-*Return:
+*@i:int to be cheked for last digit
+*@j:modulo for i
+*@k: to be returned
+*Return:k
 */
 
 int print_last_digit(int i)
 {
+int j;
+int k;
 
+j = i % 10;
+k = _putchar(j + '0');
+return (k);
 }
