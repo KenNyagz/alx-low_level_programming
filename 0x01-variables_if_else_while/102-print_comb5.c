dd4766cd219a1a9c6789cb5d315a798c6af2f5bb
KenNyagz <kennyagz681@gmail.com>
2023-06-21 12:38:31 +0300

    An aliased Commit to save time

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*Main - Entry point
+*
+*
+*Return:Always 0
+*/
+
+int main(void)
+{
+int i;
+int j;
+int k;
+int l;
+
+for (i = 48; i < 58; i++)
+{
+
+
+
+
+}
+
+
+
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*print_last_digit - prints the last digit of a number
+*
+*Return:
+*/
+
+int print_last_digit(int i)
+{
+
+}
