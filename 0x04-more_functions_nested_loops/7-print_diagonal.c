ac78654283345044316fcd3e092d1f88dfc726b0
KenNyagz <kennyagz681@gmail.com>
2023-06-23 02:03:06 +0300

    An aliased Commit to save time

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,16 +18,14 @@ else
 {
 int i, j;
 
-for (i = 1; i <= n; i++)
+for (i = 0; i < n; i++)
 {
-for (j = 1; j <= n; j++)
-if (j == i)
-_putchar('\\');
-else if (j < i)
+for (j = 0; j < i; j++)
 _putchar(' ');
-
-}
+_putchar('\\');
+_putchar('\n');
 }
 
 _putchar('\n');
+}
 }
