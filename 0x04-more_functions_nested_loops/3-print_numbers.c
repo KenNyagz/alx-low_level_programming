8f58c2c74a66b65de9e4dd1c435d72559aeb567a
KenNyagz <kennyagz681@gmail.com>
2023-06-22 14:40:13 +0300

    An aliased Commit to save time

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
 *print_numbers - prints 0 - 9
-*@i: automatic var to initiate print
+*
 *return: void
 */
 
@@ -12,6 +12,6 @@ int i;
 for (i = 0; i <= 9; i++)
 {
 _putchar(i + '0');
-} 
+}
 _putchar('\n');
 }
