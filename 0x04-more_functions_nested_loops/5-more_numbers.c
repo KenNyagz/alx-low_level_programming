bd74aacbb1397d1136ccad589cc0c4ceb765bd37
KenNyagz <kennyagz681@gmail.com>
2023-06-22 17:18:39 +0300

    An aliased Commit to save time

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,26 +2,24 @@
 
 /**
 *more_numbers - print 0-10 14 times
-*@i:int to be printed
-*@j:iteration
 *Return: void
 */
 
 void more_numbers(void)
 {
-int i;
-int j,k;
+int i, j, k;
+
 int l = 0;
-while (l<10)
+while (l < 10)
 {
-for (i = 0; i <= 14; i ++)
+for (i = 0; i <= 14; i++)
 {
-j = i/10;
-k = i%10;
+j = i / 10;
+k = i % 10;
 
 _putchar(j + '0');
 _putchar(k + '0');
-_putchar(',');    
+_putchar(',');
 
 }
 l++;
