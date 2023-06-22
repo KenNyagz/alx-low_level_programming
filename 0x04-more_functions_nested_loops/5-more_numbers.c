f30711cf54bccfeb16c8336c570e01fed89285d9
KenNyagz <kennyagz681@gmail.com>
2023-06-22 18:52:15 +0300

    print 1-14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,18 +8,20 @@
 void more_numbers(void)
 {
 int i, j, k;
-
 int l = 0;
-while (l < 10)
-{
-for (i = 0; i <= 14; i++)
-{
-j = i / 10;
-k = i % 10;
 
+while (l<10)
+{
+for (i = 0; i <= 14; i ++)
+{
+j = i/10;
+k = i%10;
+if (j > 0)
 _putchar(j + '0');
 _putchar(k + '0');
-_putchar(',');
+if(j == 1 && k == 4 )
+continue;
+_putchar(',');    
 
 }
 l++;
