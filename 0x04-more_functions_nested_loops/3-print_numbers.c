7fa88465e7d3d4ff812a36237f09032e3f6693a9
KenNyagz <kennyagz681@gmail.com>
2023-06-22 14:02:15 +0300

    Print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_numbers - prints 0 - 9
+*@i: automatic var to initiate print
+*return: void
+*/
+
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + '0');
+} 
+}
