0305339c6c63d0adceff2c2a51ab695932fbc777
KenNyagz <kennyagz681@gmail.com>
2023-06-23 01:16:16 +0300

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,11 +2,32 @@
 
 /**
 *print_diagonal - print several \ to form diagonal on terminal
-*
+*@n: int to set diagonal size
 *Return:void
 */
 
-void print_diagonal(int n);
+void print_diagonal(int n)
 {
 
+
+if (n < 1 || n < 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
+
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j <= n; j++)
+if (j == i)
+_putchar('\\');
+else if (j < i)
+_putchar(' ');
+
+}
+}
+
+_putchar('\n');
 }
