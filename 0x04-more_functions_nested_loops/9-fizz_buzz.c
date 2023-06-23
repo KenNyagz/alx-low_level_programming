add7b5deebcb156589b07232d3a62c09f4ab433c
KenNyagz <kennyagz681@gmail.com>
2023-06-24 00:36:10 +0300

    An aliased Commit to save time

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,32 +5,32 @@
 *main -entry point
 *print 1-100 but print fizz on multples of 3
 *print buzz on multiples of 5 and FizzBuzz on multiples of both
-*Return: O
+*Return:void
 */
 
-int main(void)
+void main(void)
 {
 int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 15 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else if (i % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
 }
+printf(" ");
+}
+printf("%c", '\n');
 
-}
-printf("%c ", '\n');
-return (0);
 }
