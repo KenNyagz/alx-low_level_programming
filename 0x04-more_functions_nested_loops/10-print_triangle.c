ce5267c76a28ebce14a4700135035dbd35a63d82
KenNyagz <kennyagz681@gmail.com>
2023-06-24 01:07:45 +0300

    An aliased Commit to save time

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*print_triangles - uses '#' to print triangle on th e terminal
+*@size: var to set triangle height
+*Return: void
+*/
+
+void print_triangles(int size)
+{
+int i;	
+
+}
