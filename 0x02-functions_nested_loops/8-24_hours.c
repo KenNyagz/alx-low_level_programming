52484aa3e077ebefa2562340a0cf7310fa424506
KenNyagz <kennyagz681@gmail.com>
2023-06-24 18:21:29 +0300

    24hours 59minutes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,33 @@
 #include "main.h"
 
 /**
-*jack_bauer =
+*jack_bauer - prints every hour and minute in a day
 *
-*Return
+*Return: void
 */
 
-void jack_bauer()
+void jack_bauer(void)
 {
-int i,j,k,l;
-for int i:
+int f, i, j, k, l, m;
+for (i = 0; i < 24; i++)
+{
+for (m = 0; m < 60; m++)
+{
 
+k = m / 10;
+l = m % 10;
+f = i / 10;
+j = i % 10;
+
+_putchar(f + '0');
+_putchar(j + '0');
+_putchar (':');
+_putchar(k + '0');
+_putchar(l + '0');
+_putchar('\n');
+}
+}
+_putchar('\n');
 
 }
-
 
