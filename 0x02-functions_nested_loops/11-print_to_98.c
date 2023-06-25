05285b7d920f596e806e0c3e4a5396a7727b35fb
KenNyagz <kennyagz681@gmail.com>
2023-06-26 01:42:34 +0300

    Print to 98 from n

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*print_to_98- print numbers from starting point to 98
+*separated by commas and whitespaces
+*@n: starting point
+*Return: void
+*/
+
+void print_to_98(int n)
+{
+
+
+}
