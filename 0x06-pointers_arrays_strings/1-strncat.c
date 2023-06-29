d17778ebdb57bba89a8e3ba0f944d2b7664bce46
KenNyagz <kennyagz681@gmail.com>
2023-06-29 16:04:19 +0300

    Concatenate string upto n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* *_strncat - Concatenate two strings but limit to n bytes from src
+*@dest: First string(Being concatenated to)
+*@src: second string(to be added to  first)
+*@n: - number of chars from src to be added to dest
+*Return: char *dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+
+while (dest[i] != '\0')
+i++;
+
+while (src[j] != '\0')
+{
+if (j <= n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+}
+dest[i] = '\0';
+return (dest);
+}
