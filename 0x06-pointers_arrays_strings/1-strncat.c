246fbcd7a9fb96463946466ae5b42e70a6bb5859
KenNyagz <kennyagz681@gmail.com>
2023-06-29 19:54:56 +0300

    An aliased Commit to save time

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,18 +13,17 @@ char *_strncat(char *dest, char *src, int n)
 int i = 0;
 int j = 0;
 
-while (dest[i] != '\0')
+while(dest[i] != '\0')
 i++;
 
-while (src[j] != '\0')
-{
-if (j <= n)
+while (src[j] != '\0' && n > 0)
 {
 dest[i] = src[j];
 i++;
 j++;
-}
+n--;
 }
 dest[i] = '\0';
+
 return (dest);
 }
