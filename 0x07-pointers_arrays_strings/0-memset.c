548dd88688c79ae54ceff467412114a35a01c18f
KenNyagz <kennyagz681@gmail.com>
2023-07-03 16:46:27 +0300

    A file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_memset - fills memory with a constant byte
+*@s: the memroy block(of chars
+*@b: the char byte being filled constantly
+*@n: number of bytes being filled
+*Return: char *s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+s[i] = b;
+
+return (s);
+}
