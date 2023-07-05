63f1f7850eff9c5be9200bda2bbe1565d1c9cc1e
KenNyagz <kennyagz681@gmail.com>
2023-07-05 19:41:04 +0300

    An aliased Commit to save time

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,10 @@
 
 int _strlen_recursion(char *s)
 {
-int count = 0;
 
-if (*s != '\0')
-{
-count++;
-count = _strlen_recursion(s + 1);
-}
-return (count);
+if (*s == '\0')
+return (0);
+
+s++;
+return (1 + _strlen_recursion(s));
 }
