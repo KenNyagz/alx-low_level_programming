b68afbb8d7f7b3f91a862c75946271222c871bf0
KenNyagz <kennyagz681@gmail.com>
2023-07-05 22:06:08 +0300

    Power with recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
 *_pow_recursion - raises x to the power of y
 *@x: the base number
 *@y: the power
-*Return : x raised to power y; -1 if y <0
+*Return: x raised to power y, -1 if y <0
 */
 
 int _pow_recursion(int x, int y)
@@ -15,6 +15,6 @@ return (-1);
 if (y == 0)
 return (1);
 
-return(x * _pow_recursion(x, y - 1));
+return (x * _pow_recursion(x, y - 1));
 
 }
