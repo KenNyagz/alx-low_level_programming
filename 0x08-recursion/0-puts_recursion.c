b70f21bae789e89f3cf87ffdeb2a9b5a3e23a0b1
KenNyagz <kennyagz681@gmail.com>
2023-07-05 15:14:05 +0300

    An aliased Commit to save time

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*_puts_recusrion - prints a string
+*_puts_recursion - prints a string
 *@s: the string
 *Return: Void
 */
@@ -9,10 +9,11 @@
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
+{
+_putchar('\n');
 return;
+}
 _putchar(*s);
 s++;
 _puts_recursion(s);
-_putchar('\n');
-
 }
