5ae696d2d364ee209343fe3dc28285856cfe08d2
KenNyagz <kennyagz681@gmail.com>
2023-07-07 19:34:35 +0300

    Function that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*
+*
+*
+*/
+
+int main(int argc, char *argv[])
+{
+int i;
+while (argv[i] = 0);
+for (j = 0; j < strlen(argv); j++)
+_putchar(argv[j]);
+
+}
