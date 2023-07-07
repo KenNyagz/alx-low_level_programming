857f631aa8d49b847542d957829bcf184d3dd0f8
KenNyagz <kennyagz681@gmail.com>
2023-07-07 19:38:24 +0300

    An aliased Commit to save time

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,16 +2,19 @@
 #include <stdio.h>
 
 /**
-*
-*
-*
+*main -entry point
+*function prints its name
+*@argc:  argument count
+*@argv: argument array
+*Return: (0)
 */
 
 int main(int argc, char *argv[])
 {
 int i;
-while (argv[i] = 0);
+while (argv[i] = 0)
 for (j = 0; j < strlen(argv); j++)
 _putchar(argv[j]);
+return (0);
 
 }
