867e4636658aa75b8b8694fd5fe8bad0fef38a3c
KenNyagz <kennyagz681@gmail.com>
2023-07-07 23:43:04 +0300

    Get change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - entry point
+*Calculates number of coins req to make change
+*@argc: no of arguments
+*@argv: array of arguments
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+int coins, cents;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+cents = atoi(argv[1]);
+if (cents < 0)
+{
+printf("0\n");
+return (0);
+}
+
+while (cents > 0)
+{
+if (cents >= 25)
+{
+cents -=25;
+coins++;
+}
+else if (cents >= 10)
+{
+cents -= 10;
+coins++;
+}
+else if (cents >= 5)
+{
+cents -= 5;
+coins++;
+}
+else if (cents >= 2)
+{
+cents -= 2;
+coins++;
+}
+}
+return (0);
+}
