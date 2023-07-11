b551ab25cfb0b2f930dfa73db3ce1e4c5efe7571
KenNyagz <kennyagz681@gmail.com>
2023-07-11 14:52:26 +0300

    Pointer pointing to a duplicate of string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*_strdup - returns a pointer to a newly allocated space in memory
+*which is a duplicate of str
+*@str: the string to be duplicated
+*Return: NULL if str = NULL,pointer to str duplicte 
+*/
+
+char *_strdup(char *str)
+{
+char *p;
+
+if (str == NULL)
+return NULL;
+
+p = malloc((strlen(str) + 1) * sizeof(char));
+if (p == NULL)
+return NULL;
+
+strcpy(p, str);
+return (p);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,9 @@
-
-
+#ifndef _MAIN_H
+#define _MAIN_H
 
 int _putchar(char c);
 char *create_array(unsigned int, char);
+char *_strdup(char *str);
+
+
+#endif
