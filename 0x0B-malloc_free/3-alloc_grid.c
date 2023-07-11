d4bd041f31110b81f5cedaf100f4a047a9ef8282
KenNyagz <kennyagz681@gmail.com>
2023-07-12 02:07:34 +0300

    An aliased Commit to save time

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,14 +3,35 @@
 #include <string.h>
 
 /**
-*
-*
-*
-*
+*alloc_grid - return a pointer to a 2d int array
+*@width: first dimension of the array
+*@height: second dimension of the array
+*Return: NULL on failure or if width or height are 0
 */
 
 int **alloc_grid(int width, int height)
 {
+int **array2d;
+int i, j;
 
+if (width <= 0 || height <= 0)
+return (NULL);
 
+array2d = malloc(height * sizeof(int *));
+
+if (array2d == NULL)
+return (NULL);
+
+for (i = 0; i <= height; i++)
+{
+array2d[i] = malloc(width * sizeof(int));
+if (array2d[i] == NULL)
+{
+for (j = 0; j <= i; j++)
+free(array2d[j]);
+free(array2d);
+return (NULL);
+}
+}
+return (array2d);
 }
