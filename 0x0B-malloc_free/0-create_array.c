8ad8401b6f7c52cff152da51aa1797ecdfdcffed
KenNyagz <kennyagz681@gmail.com>
2023-07-11 10:29:44 +0300

    An aliased Commit to save time

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,15 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-*
-*
-*
-*
+*create_array - creates an array of chars
+*@size: the length of the array
+*@c: the char that initializes the array
+*Return: NULL if size = 0, pointer to array otherwise
 */
 
 char *create_array(unsigned int size, char c)
 {
+char arr[] = "";
+char *p = arr;
 
+if (size == 0)
+{
+return (NULL);
+}
 
+arr[0] = c;
+
+return (p);
 
 }
