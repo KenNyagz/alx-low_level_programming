05f79ed988a6761f458c00578360db53db18eca3
KenNyagz <kennyagz681@gmail.com>
2023-07-11 14:54:11 +0300

    Pointer pointing to a duplicate of string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,7 +6,7 @@
 *_strdup - returns a pointer to a newly allocated space in memory
 *which is a duplicate of str
 *@str: the string to be duplicated
-*Return: NULL if str = NULL,pointer to str duplicte 
+*Return: NULL if str = NULL,pointer to str duplicte
 */
 
 char *_strdup(char *str)
@@ -14,11 +14,11 @@ char *_strdup(char *str)
 char *p;
 
 if (str == NULL)
-return NULL;
+return (NULL);
 
 p = malloc((strlen(str) + 1) * sizeof(char));
 if (p == NULL)
-return NULL;
+return (NULL);
 
 strcpy(p, str);
 return (p);
