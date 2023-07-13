305a89e034d57bee6d79b6d19db3cc799ce9f2df
KenNyagz <kennyagz681@gmail.com>
2023-07-13 22:43:14 +0300

    Creating a range of numbers with malloc

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*array_range - creates an array of int in a range
+*@min: starting point of the range
+*@max: ending point of the range
+*Return: pointer to the created array
+*/
+
+int *array_range(int min, int max)
+{
+int *ptr;
+int i;
+
+if (min == max || min > max)
+return (NULL);
+
+ptr = malloc((max-min + 1) * sizeof(int));
+
+if (ptr == NULL)
+return (NULL);
+
+for(i = 0; min <= max; i++, min++)
+{
+ptr[i] = min;
+}
+
+return (ptr);
+}
