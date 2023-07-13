27c6f4a85d0e9113b39eca7be1d4b9429667107a
KenNyagz <kennyagz681@gmail.com>
2023-07-13 18:48:53 +0300

    An aliased Commit to save time

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-
+#include <string.h>
 
 /**
 *calloc - alloacates memory for an array
@@ -18,7 +18,7 @@ return (NULL);
 if (ptr == NULL)
 return (NULL);
 
-memset(ptr, 0, nmemb, size);
+memset(ptr, 0, nmemb * size);
 
 return (ptr);
 }
