95f8f82db95d2ebb3bb1086b89287d619e39c2a1
KenNyagz <kennyagz681@gmail.com>
2023-07-13 22:44:49 +0300

    An aliased Commit to save time

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,12 +16,12 @@ int i;
 if (min == max || min > max)
 return (NULL);
 
-ptr = malloc((max-min + 1) * sizeof(int));
+ptr = malloc((max - min + 1) * sizeof(int));
 
 if (ptr == NULL)
 return (NULL);
 
-for(i = 0; min <= max; i++, min++)
+for (i = 0; min <= max; i++, min++)
 {
 ptr[i] = min;
 }
