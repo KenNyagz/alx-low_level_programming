6dd147ff9c8d694c87148ad8c767f5b7b4a934df
KenNyagz <kennyagz681@gmail.com>
2023-07-18 02:08:36 +0300

    An aliased Commit to save time

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ return;
 if (d->name == NULL)
 d->name = "(nil)";
 
-if (d->name == NULL)
+if (d->owner == NULL)
 d->owner = "(nil)";
 
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
