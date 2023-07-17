32172d19c261808dd5819d0d11ebc81193f62123
KenNyagz <kennyagz681@gmail.com>
2023-07-17 18:04:35 +0300

    Custom data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog
+#define dog
+/**
+*struct dog - custom data type
+*@name: First member
+*@age: second member
+*@owner: third member
+*Description: 
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+
+}
