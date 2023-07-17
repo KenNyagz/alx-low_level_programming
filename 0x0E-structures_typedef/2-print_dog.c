16d72534b2ec870ce09a3998e8ae290bbb362b09
KenNyagz <kennyagz681@gmail.com>
2023-07-18 01:45:42 +0300

    Access and mod members of struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+*print_dog - prints struct dog
+*@d: object of type dog
+*Return : Void
+*
+*/
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+return;
+
+printf("Name %s\n", d-> name != NULL ? d->name : "(nil)");
+printf("Age: %f\n", d->age);
+
+printf("Owner: %s\n", d->owner != NULL ? d->owner: "(nil)");
+
+}
