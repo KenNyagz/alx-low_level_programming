32ba7fe8bab4a23de3a780d8b7f0cf6e4fba6745
KenNyagz <kennyagz681@gmail.com>
2023-07-18 14:44:30 +0300

    Freeing Struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+*new_dog - creates a new object of type dog_t
+*@name: member of new_dog
+*@age: member of new_dog
+*@owner: member of new_dog
+*Return: pointer to struct new_dog
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+dog_t *new_dog;
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+return (NULL);
+
+(*new_dog).name = name;
+(*new_dog).age = age;
+(*new_dog).owner = owner;
+
+new_dog->name = malloc((strlen(name) + 1) * sizeof(char));
+if(new_dog->name == NULL)
+{
+free(new_dog);
+return (NULL);
+}
+
+strcpy(new_dog->name, name);
+
+new_dog->owner = malloc((strlen(name) + 1) * sizeof(char));
+if(new_dog->owner == NULL)
+{
+free(new_dog);
+return (NULL);
+}
+
+strcpy(new_dog->owner, owner);
+
+return (new_dog);
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+#include "dog.h"
+
+/**
+*free_dog - frees memory occpied by struct dog
+*@d: Pointer to memory block to be freed
+*Return: Void
+*/
+
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+free(d);
+
+}
