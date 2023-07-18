f5d461b90fc84314829443c02c56fbce0ab90031
KenNyagz <kennyagz681@gmail.com>
2023-07-18 22:37:26 +0300

    An aliased Commit to save time

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,15 +15,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 dog_t *new_dog;
 new_dog = malloc(sizeof(dog_t));
+
 if (new_dog == NULL)
 return (NULL);
 
-(*new_dog).name = name;
-(*new_dog).age = age;
-(*new_dog).owner = owner;
-
 new_dog->name = malloc((strlen(name) + 1) * sizeof(char));
-if(new_dog->name == NULL)
+if (new_dog->name == NULL)
 {
 free(new_dog);
 return (NULL);
@@ -32,13 +29,15 @@ return (NULL);
 strcpy(new_dog->name, name);
 
 new_dog->owner = malloc((strlen(name) + 1) * sizeof(char));
-if(new_dog->owner == NULL)
+if (new_dog->owner == NULL)
 {
+free(new_dog);
 free(new_dog);
 return (NULL);
 }
 
 strcpy(new_dog->owner, owner);
+new_dog->age = age;
 
 return (new_dog);
 }
