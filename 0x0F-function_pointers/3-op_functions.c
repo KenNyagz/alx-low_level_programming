fdb7572e00844c943a0f058bc38952c9738cbeee
KenNyagz <kennyagz681@gmail.com>
2023-07-20 02:51:11 +0300

    An aliased Commit to save time

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
 *op_add - adds two ints
@@ -45,6 +47,11 @@ return ((a) * (b));
 
 int op_div(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return ((a) / (b));
 }
 
@@ -57,5 +64,10 @@ return ((a) / (b));
 
 int op_mod(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return ((a) % (b));
 }
