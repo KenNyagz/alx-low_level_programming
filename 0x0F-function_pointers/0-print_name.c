5ea6d58aece95a098a1ad6637a966326b9f85533
KenNyagz <kennyagz681@gmail.com>
2023-07-19 13:14:46 +0300

    An aliased Commit to save time

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,14 +3,29 @@
 #include <stdio.h>
 
 /**
+*f - iterates and prints a name
+*@name: pointer to string with the name
+*
+*Return: void
+*/
+
+void f(char *name)
+{
+while (name)
+{
+printf("%s\n", name);
+}
+}
+
+/**
 *print_name - prints a name
 *@name: pointer to string with the name
 *@f: function that prints name
 *Return: void
 */
 
-void print_name(char *name, void (*f)(char*))
+void print_name(char *name, void (*f)(char *))
 {
-
+f(name);
 
 }
