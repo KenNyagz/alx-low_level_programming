1fb3cabe071cd0ffee074c7a18278fc90c80b2f5
KenNyagz <kennyagz681@gmail.com>
2023-07-20 18:15:33 +0300

    Function that prints anything fed to it as an argument

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+*print_all - function that prints anything
+*@format: list of types of args passed to func
+*@... : additional funtions
+*Return: Void
+*/
+
+void print_all(const char * const format, ...)
+{
+
+
+
+
+}
