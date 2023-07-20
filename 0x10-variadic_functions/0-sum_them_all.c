6701be8a24da8b5e7814982ec027a193747cd949
KenNyagz <kennyagz681@gmail.com>
2023-07-20 13:35:13 +0300

    Variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "variable_functions.h"
+
+/**
+*sum_them_all -
+*@n: Constant variable
+*
+*Return: 0
+*
+*/
+
+int sum_them_all(const unsigned int n, ....)
+{
+
+
+
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,2 @@
+Now onto variadic functions.
+These are functions that can take variable number of argumnents
