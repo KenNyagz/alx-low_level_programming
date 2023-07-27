0f08a963e101c5f601994747419fe940440df069
KenNyagz <kennyagz681@gmail.com>
2023-07-27 14:29:27 +0300

    Now on Simply linked lists

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+Now doing singly linked lists.
+
+A combination of structs(Structures) and pointers to create nodes.
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,9 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+
+#endif
