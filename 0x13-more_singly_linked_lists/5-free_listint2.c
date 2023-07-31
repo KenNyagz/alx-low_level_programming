9ff9a3146c6f13cbc1f0ef964c3c8fe36261e89c
KenNyagz <kennyagz681@gmail.com>
2023-07-31 17:04:53 +0300

    Free node(Using pointer to pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+*free_list - frees memory occupied by struct listint_s
+*@head: point to linked list, first element
+*
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *curr = *head;
+
+while (curr != NULL)
+{
+curr = curr->next;
+free(*head);
+*head = curr;
+}
+}
