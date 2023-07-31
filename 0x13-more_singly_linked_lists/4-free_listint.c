aa9e8a05a9d074a6c447451da4306aa1145691a7
KenNyagz <kennyagz681@gmail.com>
2023-07-31 16:53:55 +0300

    Freeing node

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+*free_listint - frees memory occupied by a struct listint_s
+*@head: pointer to list to be freed
+*Return: void
+*/
+
+void free_listint(listint_t *head)
+{
+listint_t *curr = head;
+
+while (curr != NULL)
+{
+curr = curr->next;
+free (head);
+head = curr;
+}
+
+}
