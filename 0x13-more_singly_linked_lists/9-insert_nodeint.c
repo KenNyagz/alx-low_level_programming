f19eb1763f3d5437a3f0f98d1208cf147891b9c0
KenNyagz <kennyagz681@gmail.com>
2023-08-01 11:15:50 +0300

    An aliased Commit to save time

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,18 +13,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 size_t i = 0;
 listint_t *newnode;
 listint_t *current = *head;
-newnode = malloc(sizeof(listint_t));
-
-if (newnode == NULL)
-return (NULL);
 
 if (idx == 0)
 {
-/*newnode = malloc(sizeof(listint_t));*/
+newnode = malloc(sizeof(listint_t));
 if (newnode == NULL)
 return (NULL);
 
-current->next = newnode;
 newnode->next = *head;
 newnode->n = n;
 *head = newnode;
@@ -33,12 +28,13 @@ return (newnode);
 
 while (current != NULL && i < idx)
 {
-if (current == NULL)
-return (NULL);
 current = current->next;
 i++;
 }
 
+newnode = malloc(sizeof(listint_t));
+if (current == NULL)
+return (NULL);
 newnode->n = n;
 newnode->next =  current->next;
 current->next = newnode;
