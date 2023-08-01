1b2b9ca3ffb61574c8f8350186a53b90f7d9e2bd
KenNyagz <kennyagz681@gmail.com>
2023-08-01 23:35:15 +0300

    An aliased Commit to save time

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,15 +9,25 @@
 size_t print_listint_safe(const listint_t *head)
 {
 size_t count = 0;
-const listint_t *temp = head;
+const listint_t *slow = head;
+const listint_t *fast = head;
 
-while (temp != NULL)
+while (slow != NULL && fast != NULL && fast->next != NULL)
 {
-printf("[%p]", (void *)(*temp).next);
-printf("%d\n", temp->n);
+slow = slow->next;
+fast = fast->next->next;
 
+printf("[%p] %d\n", (void *)slow->next, slow->n);
 count++;
-temp = temp->next;
+
+if (slow == fast)
+{
+printf("->  [%p] %d\n", (void *)fast->next, fast->next->n);
+printf("->  [%p] %d\n", (void *)fast->next->next, fast->next->next->n);
+
+exit (98);
+}
+
 }
 
 return (count);
