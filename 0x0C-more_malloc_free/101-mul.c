831d31597f5ed68cf90893107eea06e0004ad36e
KenNyagz <kennyagz681@gmail.com>
2023-08-02 14:07:07 +0300

    An aliased Commit to save time

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -49,6 +49,6 @@ if (!is_positive_num(argv[1]) && !is_positive_num(argv[2]))
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 
-printf("%d\n", (num1 *num2));
+printf("%d\n", ((num1) *(num2));
 return (0);
 }
