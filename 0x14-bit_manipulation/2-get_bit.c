de69ff54ab390da4bb002ab8dce47813d940f1af
KenNyagz <kennyagz681@gmail.com>
2023-08-04 11:51:58 +0300

    An aliased Commit to save time

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,11 +10,12 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned int mask = 1 << index;
+int bitval;
 
 if (index > sizeof(n) * 8)
 return (-1);
 
-int bitval = (n & mask) ? 1 : 0;
+bitval = (n & mask) ? 1 : 0;
 
 return (bitval);
 }
