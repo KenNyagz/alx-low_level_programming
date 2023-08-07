5430990dc11ba2b243aad217484274cbd9f73399
KenNyagz <kennyagz681@gmail.com>
2023-08-07 18:38:56 +0300

    Function that creates a file with specified permissions

--- 0x15-file_io/1-in_create_file.c
+++ 0x15-file_io/1-in_create_file.c
@@ -9,31 +9,23 @@
 
 int create_file(const char *filename, char *text_content)
 {
-int fd;
-size_t text_length;
-ssize_t byteswritten;
+FILE *file;
 
 if (filename == NULL)
 return (-1);
 
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-if (fd == -1)
+file = fopen(filename, "w");
+
+if (file == NULL)
 return (-1);
 
 if (text_content != NULL)
+if(fprintf(file, "%s", text_content) < 0)
 {
-text_length = 0;
-while (text_content[text_length] != '\0')
-text_length++;
-}
-byteswritten = write(fd, text_content, text_length);
-close(fd);
-
-if (byteswritten != (ssize_t)text_length)
+fclose(file);
 return (-1);
+}
 
-else
-close(fd);
-
+fclose(file);
 return (1);
 }
