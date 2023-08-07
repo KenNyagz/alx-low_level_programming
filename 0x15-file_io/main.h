d0627519c3d682b2bf60c122e838003dd88b708b
KenNyagz <kennyagz681@gmail.com>
2023-08-07 16:30:04 +0300

    Read and prints all chars in a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
 int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letter);
 
 #endif
