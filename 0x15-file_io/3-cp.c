c4b3d91d7f39701db34a9214e3ef38a1b152bae0
KenNyagz <kennyagz681@gmail.com>
2023-08-18 11:22:27 +0300

    An aliased Commit to save time

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,22 +17,26 @@ int main (int argc, char *argv[])
 int sourcefd, destfd;
 ssize_t bytesread, byteswritten;
 char buffer[BUFFERSIZE];
-char *source = argv[1], *dest = argv[2];
+/*char *source = argv[1], *dest = argv[2];*/
 
 if (argc != 3)
 {
-handleError(97,"Usage: cp file_from file_to");
+dprintf(STDERR_FILENO,"Usage: %s file_from file_to\n", argv[0]);
 }
 
-sourcefd = open(source, O_RDONLY);
+sourcefd = open(argv[1], O_RDONLY);
 if(sourcefd == -1)
-handleError(98, strerror(errno));
+{
+dprintf(STDERR_FILENO,"Error: Can't read form file %s\n", argv[1]);
+exit(98);
+}
 
-destfd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+destfd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (destfd == -1)
 {
 close(sourcefd);
-handleError(99, strerror(errno));
+dprintf(STDERR_FILENO, "Can't write to %s\n", argv[2]);
+exit(99);
 }
 
 while ((bytesread = read(sourcefd, buffer, BUFFERSIZE)) > 0)
