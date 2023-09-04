9a091d9d95d2e213e8c261a1e40241deb72b3cb2
KenNyagz <kennyagz681@gmail.com>
2023-09-05 01:17:01 +0300

    An aliased Commit to save time

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,7 +28,7 @@ char buffer[BUFFERSIZE], *source = argv[1], *dest = argv[2];
 		exit(98);
 	}
 	destfd = open(dest, O_WRONLY | O_CREAT | O_TRUNC,
-S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (destfd == -1)
 	{
 		dprintf(STDERR_FILENO, "Can't write to %s\n", dest);
