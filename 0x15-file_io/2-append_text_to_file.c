e8a9eb8d643d36523b4f1264cc78509ef76a36e3
KenNyagz <kennyagz681@gmail.com>
2023-09-04 21:10:04 +0300

    An aliased Commit to save time

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,32 +9,35 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-ssize_t byteswritten;
-size_t text_length = strlen(text_content);
-int fd;
+/*
+*ssize_t byteswritten;
+*size_t text_length = strlen(text_content);
+*int fd;
+*/
+int success;
+FILE *file;
 
 if (filename == NULL)
 return (-1);
 
 if (text_content == NULL)
-{
-if (filename == NULL)
-return (-1);
-else
 return (1);
-}
 
-fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-if (fd == -1)
+/*fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);*/
+file = fopen(filename, "a");
+if (file == NULL)
 {
 return (-1);
 }
-
-byteswritten = write(fd, text_content, text_length);
-close(fd);
-
-if (byteswritten < (ssize_t)text_length)
-return (-1);
-
-return (1);
+success = fputs(text_content, file);
+/*
+*byteswritten = write(fd, text_content, text_length);
+*close(fd);
+*
+*if (byteswritten < (ssize_t)text_length)
+*return (-1);
+*/
+fclose(file);
+/*return (1);*/
+return ((success == EOF) ? -1 : 1);
 }
