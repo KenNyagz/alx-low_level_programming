723f8ef754ae3068598dda91bf75ddab4db4edf1
KenNyagz <kennyagz681@gmail.com>
2023-09-05 18:06:50 +0300

    An aliased Commit to save time

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,116 +1,86 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "main.h"
 #include <elf.h>
 
-void print_elf_info(Elf64_Ehdr *header) {
-int i;
-printf("  Magic:   ");
-for (i = 0; i < EI_NIDENT; i++) 
+void print_error(const char *);
+void print_elf_header_info(Elf64_Ehdr *header);
+void read_elf_header(const char *filename);
+
+/**
+*main - checks and prints elf header of a file
+*@argc: argument count
+*@argv: arguments strings
+*Return: 0 ,success
+*/
+
+int main(int argc, char *argv[])
 {
-printf("%02x ", header->e_ident[i]);
-}
-printf("\n");
-printf("  Class:                             ");
-switch (header->e_ident[EI_CLASS]) {
-case ELFCLASS32:
-printf("ELF32\n");
-break;
-case ELFCLASS64:
-printf("ELF64\n");
-break;
-default:
-printf("Invalid\n");
-break;
-}
-printf("  Data:                              ");
-switch (header->e_ident[EI_DATA]) {
-case ELFDATA2LSB:
-printf("2's complement, little endian\n");
-break;
-case ELFDATA2MSB:
-printf("2's complement, big endian\n");
-break;
-default:
-printf("Invalid\n");
-break;
-}
-printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+if (argc != 2)
+	print_error("Usage: elf_header elf_filename");
 
-printf("  OS/ABI:                            ");
-switch (header->e_ident[EI_OSABI]) {
-case ELFOSABI_SYSV:
-printf("UNIX - System V\n");
-break;
-case ELFOSABI_HPUX:
-printf("UNIX - HP-UX\n");
-break;
-case ELFOSABI_NETBSD:
-printf("UNIX - NetBSD\n");
-break;
-case ELFOSABI_LINUX:
-printf("UNIX - Linux\n");
-break;
-default:
-printf("Unknown\n");
-break;
+read_elf_header(argv[1]);
+
+return (0);
 }
 
-printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-
-printf("  Type:                              ");
-switch (header->e_type) {
-case ET_NONE:
-printf("NONE (Unknown)\n");
-break;
-case ET_REL:
-printf("REL (Relocatable file)\n");
-break;
-case ET_EXEC:
-printf("EXEC (Executable file)\n");
-break;
-case ET_DYN:
-printf("DYN (Shared object file)\n");
-break;
-case ET_CORE:
-printf("CORE (Core file)\n");
-break;
-default:
-printf("Unknown\n");
-break;
-}
-
-printf("  Entry point address:               0x%lu\n", (long unsigned int)header->e_entry);
-}
-
-int main(int argc, char *argv[]) {
-Elf64_Ehdr elf_header;
-const char *filename;
-int fd;
-ssize_t bytes_read;
-if (argc != 2) {
-fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+/**
+*print_error - prints error message
+*@message: str to be printed
+*Return: void
+*/
+void print_error(const char *message)
+{
+fprintf(stderr, "Error: %s\n", message);
 exit(98);
 }
 
-filename = argv[1];
-fd = open(filename, O_RDONLY);
-if (fd == -1) {
-fprintf(stderr, "Error: Cannot open file '%s'\n", filename);
-exit(98);
+/**
+*print_elf_header_info - prints elf header file info
+*@header: the elf header
+*Return: void
+*/
+void print_elf_header_info(Elf64_Ehdr *header)
+{
+int i;
+printf("Magic: ");
+	for (i = 0; i < 16; i++)
+		printf("%02x ", header->e_ident[i]);
+
+	printf("\n");
+	printf("Class: %d\n", header->e_ident[EI_CLASS]);
+	printf("Data: %d\n", header->e_ident[EI_DATA]);
+	printf("Version: %d\n", header->e_ident[EI_VERSION]);
+	printf("OS/ABI: %d\n", header->e_ident[EI_OSABI]);
+	printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
+		printf("Type: %d\n", header->e_type);
+	printf("Entry point address: %lx\n", (unsigned long)header->e_entry);
 }
 
-bytes_read = read(fd, &elf_header, sizeof(elf_header));
-if (bytes_read != sizeof(elf_header) || elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
-elf_header.e_ident[EI_MAG1] != ELFMAG1 || elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
-elf_header.e_ident[EI_MAG3] != ELFMAG3) {
-fprintf(stderr, "Error: Not an ELF file: '%s'\n", filename);
+/**
+*read_elf_header - read files elf header
+*@filename: file
+*Return: void
+*/
+void read_elf_header(const char *filename)
+{
+Elf64_Ehdr header;
+int fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		print_error("Failed to open file");
+
+	if (read(fd, &header, sizeof(header)) != sizeof(header))
+	{
+		close(fd);
+		print_error("Failed to read ELF header");
+	}
+	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+		header.e_ident[EI_MAG1] != ELFMAG1 ||
+		header.e_ident[EI_MAG2] != ELFMAG2 ||
+		header.e_ident[EI_MAG3] != ELFMAG3)
+	{
+		close(fd);
+		print_error("Not an ELF file");
+	}
+
+print_elf_header_info(&header);
 close(fd);
-exit(98);
-}
-printf("ELF Header:\n");
-print_elf_info(&elf_header);
-close(fd);
-return 0;
 }
