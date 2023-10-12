4f53f8de30ba8ed161592b9ac43ce6cbeb035800
KenNyagz <kennyagz681@gmail.com>
2023-10-12 10:34:48 +0300

    No of nodes in linke list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,6 +11,8 @@ size_t print_dlistint(const dlistint_t *h)
 const dlistint_t *current = h;
 ssize_t count = 0;
 
+	if (h == NULL)
+		return (0);
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
