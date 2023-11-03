382b388da44162864d1f224f7a871da2865611b4
KenNyagz <kennyagz681@gmail.com>
2023-11-03 21:21:19 +0300

    An aliased Commit to save time

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,12 +36,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 		node->value = NULL;
 	}
-	if (!ht->array[index])
-		ht->array[index] = node;
-	else
-	{
-		node->next = ht->array[index];
-		ht->array[index] = node;
-	}
+
+	node->next = ht->array[index];
+	ht->array[index] = node;
+
 	return (1);
 }
