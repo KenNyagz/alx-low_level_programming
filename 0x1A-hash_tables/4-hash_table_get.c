d14cb8e7521ed1664d0306f68a6f7d480d76ea26
KenNyagz <kennyagz681@gmail.com>
2023-11-03 22:36:23 +0300

    Hash get

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,8 +9,10 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned int index = 0;
 
+	index = hash_djb2((const char *key), ht->size);
 	
 
-
+	return (ht->array[index];
 }
