f1a51cd6a030f5750154e98dfeea9e4be14cdcf4
KenNyagz <kennyagz681@gmail.com>
2023-11-04 01:30:26 +0300

    An aliased Commit to save time

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,7 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 hash_node_t *node = NULL;
 unsigned int i = 0;
 
-if (ht == NULL)
+if (ht == NULL || ht->array == NULL)
 	return;
 /*node = *(ht->array);*/
 printf("{");
