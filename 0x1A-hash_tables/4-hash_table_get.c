17b271424fa114214c722e89ed3ea1573e90ad51
KenNyagz <kennyagz681@gmail.com>
2023-11-04 00:16:57 +0300

    An aliased Commit to save time

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,9 +10,16 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned int index = 0;
+	hash_node_t *temp = NULL;
 
 	index = hash_djb2((const unsigned char *)key) % ht->size;
-	
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return temp->value;
+		temp = temp->next;
+	}
 
-	return (ht->array[index]->value);
+	return (NULL);
 }
