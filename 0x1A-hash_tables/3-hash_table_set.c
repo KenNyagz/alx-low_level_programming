37a5a08328c4a54772a50ce811430afc183951f8
KenNyagz <kennyagz681@gmail.com>
2023-11-03 23:11:57 +0300

    An aliased Commit to save time

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,17 +1,20 @@
 #include "hash_tables.h"
 
 /**
-*
-*
-*
-*
+*hash_table_set - adds an element to the hash table
+*@ht: the hash table you want to add or update the key/value to
+*@key: is the key
+*@value:  is the value associated with the key
+*Return: 1 if it succeeded, 0 otherwise
 */
 
-int hash_table_set(hash_table_t* ht, const char* key, const char* value)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned int index = 0;
 hash_node_t *node = NULL;
 
+if (ht == NULL || key == NULL)
+	return (0);
 node = malloc(sizeof(hash_node_t));
 node->key = strdup(key);
 node->value = strdup(value);
