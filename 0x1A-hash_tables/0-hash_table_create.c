910a21a129b840d7467003061ccae17a1cb57abf
KenNyagz <kennyagz681@gmail.com>
2023-11-03 11:00:44 +0300

    Hash table create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	newtable->size = size;
-	newtable->array = calloc(size, sizeof(hash_node_t));
+	newtable->array = calloc(size,  sizeof(hash_node_t *));
 	if (newtable->array == NULL)
 	{
 		free(newtable);
