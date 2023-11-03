1bbd53903359d7d925a4c88f9b633e31fe65a36e
KenNyagz <kennyagz681@gmail.com>
2023-11-03 23:22:44 +0300

    An aliased Commit to save time

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,12 +16,18 @@ hash_node_t *node = NULL;
 if (ht == NULL || key == NULL || value == NULL)
 	return (0);
 node = malloc(sizeof(hash_node_t));
+if (node == NULL)
+	return (0);
 node->key = strdup(key);
+if (node->key == NULL)
+	return (0);
 node->value = strdup(value);
+if (node->value == NULL)
+	return (0);
 
 index = hash_djb2((const unsigned char *)key) % ht->size;
 
-	if (!ht->array[index])
+	if (ht->array[index] != NULL)
 	{
 		ht->array[index] = node;
 		node->next = NULL;
