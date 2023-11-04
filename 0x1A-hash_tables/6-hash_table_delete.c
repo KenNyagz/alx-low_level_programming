953fc0a7b74eaa448333650ba320257d9d8b36a9
KenNyagz <kennyagz681@gmail.com>
2023-11-04 14:34:42 +0300

    An aliased Commit to save time

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,7 +10,7 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *current = NULL, *current2 = NULL, **temp = NULL;
 	unsigned int i = 0;
 
-	if (ht == NULL)
+	if (ht == NULL || ht->array == NULL)
 		return;
 	temp = ht->array;
 	for (i = 0; i < ht->size; i++)
@@ -31,7 +31,7 @@ void hash_table_delete(hash_table_t *ht)
 				current2 = current->next;
 				free(current->key);
 				free(current->value);
-				free(current); 
+				free(current);
 				current = current2;
 			}
 		}
